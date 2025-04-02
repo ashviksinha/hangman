09ca80c92dd2488b063f758b0a2bdafcb6ebe1af
ashviksinha <SINHA.ASHVIK@GMAIL.COM>
2025-04-02 11:02:28 -0700

    Create words.js

---
+++ js/words.js
@@ -0,0 +1,12 @@
+const words = [
+    "apple", "banana", "cherry", "dragon", "elephant", "flower", "giraffe", "honey", "island", "jungle",
+    "kitten", "lemon", "monkey", "nest", "orange", "panda", "queen", "rabbit", "snake", "tiger",
+    "umbrella", "violet", "whale", "xylophone", "zebra", "butterfly", "cactus", "dolphin", "eagle", "flamingo",
+    "guitar", "hammer", "igloo", "jelly", "koala", "lizard", "mango", "night", "ocean", "penguin",
+    "quartz", "rose", "sunset", "turtle", "unicorn", "vulture", "water", "xenon", "yellow", "zipper",
+    "avocado", "beetle", "canyon", "desert", "emerald", "forest", "galaxy", "horizon", "insect", "jasmin",
+    "kiwi", "lotus", "meadow", "noodle", "oasis", "peacock", "quail", "river", "shadow", "thunder",
+    "utopia", "valley", "willow", "xray", "yogurt", "zombie", "bamboo", "coral", "daisy", "energy",
+    "fossil", "glacier", "hollow", "ivory", "juice", "kernel", "lantern", "marble", "nectar", "orbit",
+    "pearl", "quilt", "reef", "saddle", "trumpet", "velvet", "waffle", "yacht", "zigzag"
+];
